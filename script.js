54e6156308d9a92bedc0afbeff33fa0c16421770
ConstintCreations <ethanglenn1208@gmail.com>
2025-10-26 12:24:49 -0500

    Multi-Tree Support and Tree Creation with Random Spawns

--- script.js
+++ script.js
@@ -1,35 +1,74 @@
-const tempTree = document.querySelector('.tree');
-let animation = {
-    growing: "treeGrowAnimation 1.5s forwards",
-    falling: "treeFallAnimation 3s ease-in forwards"
+const main = document.querySelector('.main');
+
+const trees = [];
+
+const wood = {
+    temporary: 0,
 }
 
-const treeObject = {
-    element: tempTree,
-    type: "temporary",
-    clickable: true,
-    state: "idle", /* idle, shaking, shakingDelay, falling, growing */
-    timer: 0,
-    clickedThisShake: false,
-    clicks: 0,
-
+const treeUpgrades = {
+    woodPerClick: 1,
     maxClicks: 10,
     regrows: 1,
-    
     regrowSpeed: 5000,
     fallSpeed: 3000,
     shakeDelay: 1000,
-
-    clickedTime: 400, /* shakeDelay/2.5 */
 }
 
-treeObject.element.addEventListener('click', (e) => {
-    if (!treeObject.clickable) return;
+createTree("starter");
+createTree("temporary");
 
-    treeObject.state = "shaking";
-    treeObject.clickable = false;
-    treeObject.timer = treeObject.clickedTime;
-});
+function createTree(type) {
+    let centered = false;
+    const treeElement = document.createElement('img');
+    switch(type) {
+        case "temporary":
+            treeElement.src = 'images/treeplaceholder.png';
+            break;
+        case "starter":
+            treeElement.src = 'images/treeplaceholder.png';
+            centered = true;
+            break;
+    }
+
+    treeElement.classList.add('tree');
+
+    if (centered) {
+        treeElement.style.left = `calc(50% - 2.5vh)`;
+        treeElement.style.top = `calc(50% - 5vh)`;
+    } else {
+        treeElement.style.left = `${Math.random() * ((window.innerWidth *0.7) - 0.05 * window.innerHeight)}px`;
+        treeElement.style.top = `${Math.random() * (window.innerHeight - 0.1 * window.innerHeight)}px`;
+    }
+
+    main.appendChild(treeElement);
+
+    const treeObj = {
+        element: treeElement,
+        type: (type == "starter" ? "temporary" : type),
+        clickable: true,
+        state: "idle", /* idle, shaking, shakingDelay, falling, growing */
+        timer: 0,
+        clickedThisShake: false,
+        clicks: 0,
+        maxClicks: treeUpgrades.maxClicks,
+        regrows: treeUpgrades.regrows,
+        regrowSpeed: treeUpgrades.regrowSpeed,
+        fallSpeed: treeUpgrades.fallSpeed,
+        shakeDelay: treeUpgrades.shakeDelay,
+        clickedTime: (treeUpgrades.shakeDelay / 2.5),
+    }
+
+    treeObj.element.addEventListener('click', (e) => {
+        if (!treeObj.clickable) return;
+
+        treeObj.state = "shaking";
+        treeObj.clickable = false;
+        treeObj.timer = treeObj.clickedTime;
+    });
+
+    trees.push(treeObj);
+}
 
 
 
@@ -39,69 +78,72 @@ let lastTimestamp = 0;
 function gameLoop(timestamp) {
     const deltaTime = timestamp - lastTimestamp;
     lastTimestamp = timestamp;
-    updateTrees(deltaTime);
+    for (let tree of trees) {
+        updateTrees(tree, deltaTime);
+    }
     requestAnimationFrame(gameLoop);
 }
 
-function updateTrees(deltaTime) {
-    switch (treeObject.state) {
+function updateTrees(tree, deltaTime) {
+    switch (tree.state) {
         case "idle":
             break;
         case "shakingDelay":
-                treeObject.timer -= deltaTime;
-                if (treeObject.timer <= 0) {
-                    treeObject.timer = treeObject.clickedTime;
-                    treeObject.state = "shaking";
+                tree.timer -= deltaTime;
+                if (tree.timer <= 0) {
+                    tree.timer = tree.clickedTime;
+                    tree.state = "shaking";
                 }
             break;
         case "shaking":
-                treeObject.timer -= deltaTime;
+                tree.timer -= deltaTime;
 
-                let progress = 1 - (treeObject.timer / treeObject.clickedTime);
+                let progress = 1 - (tree.timer / tree.clickedTime);
                 let scale = 1 + 0.05 * Math.sin(progress * Math.PI);
-                treeObject.element.style.transform = `scale(${scale})`;
+                tree.element.style.transform = `scale(${scale})`;
                 
-                if (treeObject.timer <= treeObject.clickedTime/2 && !treeObject.clickedThisShake) {
-                    treeObject.clicks += 1;
-                    treeObject.clickedThisShake = true;
-                    console.log("clicked!");
+                if (tree.timer <= tree.clickedTime/2 && !tree.clickedThisShake) {
+                    tree.clicks += 1;
+                    tree.clickedThisShake = true;
+                    wood[tree.type] += treeUpgrades.woodPerClick;
+                    console.log(wood);
                 }
 
-                if (treeObject.timer <= 0) {
-                    treeObject.clickedThisShake = false;
-                    treeObject.element.style.transform = `scale(1)`;
+                if (tree.timer <= 0) {
+                    tree.clickedThisShake = false;
+                    tree.element.style.transform = `scale(1)`;
 
-                    if (treeObject.clicks >= treeObject.maxClicks) {
-                        treeObject.clicks = 0;
-                        treeObject.state = "falling";
-                        treeObject.timer = treeObject.fallSpeed;
-                        treeObject.element.style.animation = `treeFallAnimation ${treeObject.fallSpeed/1000}s ease-in forwards`;
+                    if (tree.clicks >= tree.maxClicks) {
+                        tree.clicks = 0;
+                        tree.state = "falling";
+                        tree.timer = tree.fallSpeed;
+                        tree.element.style.animation = `treeFallAnimation ${tree.fallSpeed/1000}s ease-in forwards`;
                     } else {
-                        treeObject.state = "shakingDelay";
-                        treeObject.timer = treeObject.shakeDelay;
+                        tree.state = "shakingDelay";
+                        tree.timer = tree.shakeDelay;
                     }
                 }
             break;
         case "falling":
-            treeObject.timer -= deltaTime;
-            if (treeObject.timer <= 0) {
-                if (treeObject.regrows > 0) {
-                    treeObject.state = "growing";
-                    treeObject.timer = treeObject.regrowSpeed;
-                    treeObject.regrows -= 1;
-                    treeObject.element.style.animation = `treeGrowAnimation ${treeObject.regrowSpeed/1000}s forwards`;
+            tree.timer -= deltaTime;
+            if (tree.timer <= 0) {
+                if (tree.regrows > 0) {
+                    tree.state = "growing";
+                    tree.timer = tree.regrowSpeed;
+                    tree.regrows -= 1;
+                    tree.element.style.animation = `treeGrowAnimation ${tree.regrowSpeed/1000}s forwards`;
                 } else {
-                    treeObject.state = "idle";
-                    treeObject.clickable = false;
+                    tree.state = "idle";
+                    tree.clickable = false;
                 }
             }
             break;
         case "growing":
-                treeObject.timer -= deltaTime;
-                if (treeObject.timer <= 0) {
-                    treeObject.element.style.animation = "";
-                    treeObject.state = "idle";
-                    treeObject.clickable = true;
+                tree.timer -= deltaTime;
+                if (tree.timer <= 0) {
+                    tree.element.style.animation = "";
+                    tree.state = "idle";
+                    tree.clickable = true;
                 }
             break;
     }
